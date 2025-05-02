5ced4b51465f3dcdf0d03a9bd00173ebb740bd89
Taurai Job Munodawafa <taujob1111@gmail.com>
2025-05-02 09:42:21 +0200

    m

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'Creative HR Solutions',
-        port: '3000',
+        port: '3031',
         exec_mode: 'cluster',
         instances: '1',
         script: './.output/server/index.mjs' 
